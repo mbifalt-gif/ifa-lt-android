5e028ad29937016c0d8aa811a0b00cfb88978aed
mbifalt-gif <mbifa.lt@gmail.com>
2026-09-12 20:46:33 +0300

    Add app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "lt.ifalt.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "lt.ifalt.app"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0-test"
+    }
+}
+
+dependencies {
+}
